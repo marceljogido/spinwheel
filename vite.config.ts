6d00b0166386dab1e61a77120a3f5978bad14e4f
marceljogido <marcelmarbun33@gmail.com>
2025-12-01 20:06:44 +0700

    change allowed domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      "spinwheel.digioh.id"
+    ]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
